cb8d6d103799f08cc9ce930c106e6d3cc16de0f9
hailm <hai.lemanh.1310@gmail.com>
2024-10-30 16:10:02 +0700

    feat: update

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php $data["title"] = "404"; ?>
+<?php $isHome = false; ?>
+<?php $isLogin = true; ?>
+<?php $bodyClass = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="space-h-107"></div>
+<div class="uk-container uk-text-center">
+    <img src="images/404-1.png" alt="">
+    <div class="space-h-24"></div>
+    <h1 class="uk-margin-remove fz-26 text-000 be-vietnam-pro-semibold line-height-138">Trang bạn tìm kiếm hiện không tồn tại</h1>
+    <div class="space-h-24"></div>
+    <a href="." class="page-error__btn uk-button uk-border-pill fz-16 be-vietnam-pro-semibold text-966412">
+        Quay về trang chủ
+    </a>
+</div>
+<div class="space-h-78"></div>
+<?php require "template-parts/layouts/footer.php"; ?>
