4776d595d96e1b8d66858ceaa66a9b2853f24569
hailm <hai.lemanh.1310@gmail.com>
2025-01-24 16:11:09 +0700

    feat: chitietsach/comment
    https://themes.cathtmlcss.net/2024/09/thuvienso/chi_tiet_sach.php

--- chi_tiet_sach.php
+++ chi_tiet_sach.php
@@ -604,7 +604,6 @@
     <ul class="chi_tiet_sach__tab uk-margin-remove-top uk-margin-remove-bottom uk-tab" uk-tab>
         <li><a href="#">Mô tả chi tiết</a></li>
         <li><a href="#">Thông tin bổ sung</a></li>
-        <li><a href="#">Đánh giá (5)</a></li>
     </ul>
     <div class="space-h-60"></div>
     <div class="uk-switcher fz-14">
@@ -616,7 +615,199 @@
             <p>Earum vel delectus in facilis. Voluptatum minus nobis cum temporibus perferendis est ut. Sed aut saepe ipsum animi asperiores. Nihil nihil repudiandae adipisci quis ea voluptatum dicta.</p>
         </div>
         <div>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-        <div>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur, sed do eiusmod.</div>
+    </div>
+    <div class="space-h-40"></div>
+    <ul class="chi_tiet_sach__tab uk-margin-remove-top uk-margin-remove-bottom uk-tab" uk-tab>
+        <li><a href="#">Đánh giá (5)</a></li>
+    </ul>
+    <div class="space-h-60"></div>
+    <div class="uk-grid-match uk-flex-middle" uk-grid>
+        <div class="uk-width-expand">
+            <div class="uk-text-center uk-card uk-card-body">
+                <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200" fill="none">
+                    <g clip-path="url(#clip0_12276_19273)">
+                        <path d="M93.332 184.333C139.999 184.333 177.832 146.5 177.832 99.6667C177.832 52.8333 139.999 15 93.332 15C46.6654 15 8.83203 52.8333 8.83203 99.6667C8.83203 146.5 46.6654 184.333 93.332 184.333Z" fill="#EAEEF9"/>
+                        <path d="M187.497 73.1667C191.271 73.1667 194.331 70.1073 194.331 66.3333C194.331 62.5594 191.271 59.5 187.497 59.5C183.723 59.5 180.664 62.5594 180.664 66.3333C180.664 70.1073 183.723 73.1667 187.497 73.1667Z" fill="#EAEEF9"/>
+                        <path d="M197.499 46.3333C200.076 46.3333 202.165 44.244 202.165 41.6667C202.165 39.0893 200.076 37 197.499 37C194.921 37 192.832 39.0893 192.832 41.6667C192.832 44.244 194.921 46.3333 197.499 46.3333Z" fill="#EAEEF9"/>
+                        <path d="M2.9987 78.0003C5.57603 78.0003 7.66537 75.911 7.66537 73.3337C7.66537 70.7563 5.57603 68.667 2.9987 68.667C0.421369 68.667 -1.66797 70.7563 -1.66797 73.3337C-1.66797 75.911 0.421369 78.0003 2.9987 78.0003Z" fill="#EAEEF9"/>
+                        <path d="M122.331 50.7383C151.102 50.7383 174.311 71.6624 174.311 97.3855C174.311 112.167 166.638 125.412 154.555 134.051V153.439C154.555 156.127 151.486 157.47 149.568 155.935L133.072 143.073C129.62 143.649 126.167 144.033 122.523 144.033C93.7517 144.033 70.543 123.109 70.543 97.3855C70.543 71.6624 93.7517 50.7383 122.331 50.7383Z" fill="#CBD4E0"/>
+                        <g filter="url(#filter0_d_12276_19273)">
+                            <path d="M71.504 48.4346C39.4721 48.4346 13.5781 71.6622 13.5781 100.265C13.5781 116.774 22.2095 131.555 35.636 140.961V162.461C35.636 165.341 38.8967 167.068 41.1984 165.149L59.6119 150.943C63.448 151.711 67.2842 152.095 71.3121 152.095C103.344 152.095 129.238 128.867 129.238 100.265C129.238 71.6622 103.344 48.4346 71.504 48.4346Z" fill="url(#paint0_linear_12276_19273)"/>
+                        </g>
+                        <path d="M57.2359 43.563C57.0047 43.563 56.7734 43.563 56.5421 43.563C55.3858 43.1 54.6919 41.9427 54.9232 40.7855L58.8548 28.0552C59.3174 26.8979 60.4737 26.2035 61.6301 26.435C62.7864 26.8979 63.4802 28.0552 63.249 29.2125L59.5486 41.9427C59.3174 42.8686 58.3923 43.563 57.2359 43.563Z" fill="#B5BCCE"/>
+                        <path d="M47.5214 46.3407C46.5963 46.3407 45.6712 45.6464 45.2087 44.7205L39.1957 26.4352C38.7331 25.2779 39.4269 23.8892 40.5833 23.4263C41.7396 22.9633 43.1273 23.6577 43.5898 24.815L49.6028 43.1003C50.0654 44.2576 49.3716 45.6464 48.2152 46.1093C47.984 46.3407 47.7527 46.3407 47.5214 46.3407Z" fill="#B5BCCE"/>
+                        <path d="M38.5024 50.738C37.8086 50.738 37.3461 50.5066 36.8835 50.0436L29.4829 42.8684C28.5578 41.9426 28.5578 40.5538 29.4829 39.628C30.408 38.7021 31.7956 38.7021 32.7207 39.628L40.1213 46.8032C41.0464 47.7291 41.0464 49.1178 40.1213 50.0436C39.6588 50.5066 38.965 50.738 38.5024 50.738Z" fill="#B5BCCE"/>
+                    </g>
+                    <defs>
+                        <filter id="filter0_d_12276_19273" x="-8.42188" y="37.4346" width="159.66" height="161.524" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                            <feOffset dy="11"/>
+                            <feGaussianBlur stdDeviation="11"/>
+                            <feColorMatrix type="matrix" values="0 0 0 0 0.397708 0 0 0 0 0.47749 0 0 0 0 0.575 0 0 0 0.27 0"/>
+                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_12276_19273"/>
+                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_12276_19273" result="shape"/>
+                        </filter>
+                        <linearGradient id="paint0_linear_12276_19273" x1="71.3704" y1="45.7161" x2="71.3704" y2="167.226" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#FDFEFF"/>
+                            <stop offset="0.9964" stop-color="#ECF0F5"/>
+                        </linearGradient>
+                        <clipPath id="clip0_12276_19273">
+                            <rect width="200" height="200" fill="white"/>
+                        </clipPath>
+                    </defs>
+                </svg>
+                <div class="space-h-24"></div>
+                <h2 class="uk-margin-remove text-111827 fz-20 line-height-138 be-vietnam-pro-semibold">Chưa có bình luận</h2>
+                <div class="item-8px fz-14 text-4B5563 line-height-142">Hãy là người đầu tiên khám phá cuốn sách này và chia sẻ cảm xúc của bạn với chúng tôi!</div>
+                <div class="uk-margin-top">
+                    <a href="" class="sachdamua__btn uk-button uk-button-primary uk-border-pill uk-flex-inline" style="padding-right: 17px;gap: 12px">
+                        <span class="fz-16 be-vietnam-pro-semibold" style="color: #fff;">Thêm bình luận</span>
+                        <span class="icon24 icon24--edit"></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="uk-width-auto">
+            <div class="uk-cover-container border-20">
+                <img src="images/slider4.jpeg" alt="" uk-cover="">
+                <canvas width="320" height="534"></canvas>
+            </div>
+        </div>
+    </div>
+    <div class="space-h-60"></div>
+    <div class="" uk-grid>
+        <div class="uk-width-expand">
+            <div class="uk-text-center line-height-125 text-9A96AA be-vietnam-pro-semibold">Hãy cho chúng mình biết độ yêu thích của bạn với tựa sách này nha.</div>
+            <div class="space-h-12"></div>
+            <div class="uk-child-width-auto uk-flex-center uk-grid-8" uk-grid>
+                <div>
+                    <div class="width-104px uk-text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" fill="none">
+                            <path d="M22.5 8L25.6432 17.6738H35.8148L27.5858 23.6525L30.729 33.3262L22.5 27.3475L14.271 33.3262L17.4142 23.6525L9.18521 17.6738H19.3568L22.5 8Z" fill="#E6E6E6"/>
+                        </svg>
+                        <div class="fz-14 be-vietnam-pro-medium text-9A96AA">Thất vọng</div>
+                    </div>
+                </div>
+                <div>
+                    <div class="width-104px uk-text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" fill="none">
+                            <path d="M22.5 8L25.6432 17.6738H35.8148L27.5858 23.6525L30.729 33.3262L22.5 27.3475L14.271 33.3262L17.4142 23.6525L9.18521 17.6738H19.3568L22.5 8Z" fill="#E6E6E6"/>
+                        </svg>
+                        <div class="fz-14 be-vietnam-pro-medium text-9A96AA">Không thích</div>
+                    </div>
+                </div>
+                <div>
+                    <div class="width-104px uk-text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" fill="none">
+                            <path d="M22.5 8L25.6432 17.6738H35.8148L27.5858 23.6525L30.729 33.3262L22.5 27.3475L14.271 33.3262L17.4142 23.6525L9.18521 17.6738H19.3568L22.5 8Z" fill="#E6E6E6"/>
+                        </svg>
+                        <div class="fz-14 be-vietnam-pro-medium text-9A96AA">Bình thường</div>
+                    </div>
+                </div>
+                <div>
+                    <div class="width-104px uk-text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" fill="none">
+                            <path d="M22.5 8L25.6432 17.6738H35.8148L27.5858 23.6525L30.729 33.3262L22.5 27.3475L14.271 33.3262L17.4142 23.6525L9.18521 17.6738H19.3568L22.5 8Z" fill="#E6E6E6"/>
+                        </svg>
+                        <div class="fz-14 be-vietnam-pro-medium text-9A96AA">Thích</div>
+                    </div>
+                </div>
+                <div>
+                    <div class="width-104px uk-text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 44 44" fill="none">
+                            <path d="M22.5 8L25.6432 17.6738H35.8148L27.5858 23.6525L30.729 33.3262L22.5 27.3475L14.271 33.3262L17.4142 23.6525L9.18521 17.6738H19.3568L22.5 8Z" fill="#E6E6E6"/>
+                        </svg>
+                        <div class="fz-14 be-vietnam-pro-medium text-9A96AA">Tuyệt vời</div>
+                    </div>
+                </div>
+            </div>
+            <textarea class="chi_tiet_sach__comment__textarea uk-textarea item-24px border-10 fz-14 be-vietnam-pro-semibold text-9A96AA" rows="5" placeholder="Để lại bình luận của bạn..." aria-label="Để lại bình luận của bạn..."></textarea>
+            <div class="uk-text-right item-12px">
+                <button class="thongtintaikhoan__btnSave uk-button uk-border-pill">Gửi bình luận</button>
+            </div>
+            <div class="space-h-40"></div>
+            <div>
+                <?php for ($j = 0; $j < 6; $j++) { ?>
+                    <div class="chi_tiet_sach__comment__item border-10 item-12px">
+                        <div class="uk-grid-8" uk-grid>
+                            <div class="uk-width-auto">
+                                <div class="uk-cover-container uk-border-circle">
+                                    <img src="https://picsum.photos/24/24/?random=<?= $j ?>" alt="" uk-cover>
+                                    <canvas width="24" height="24"></canvas>
+                                </div>
+                            </div>
+                            <div class="uk-width-expand">
+                                <div uk-grid>
+                                    <div class="uk-width-expand">
+                                        <div class="fz-12 be-vietnam-pro-medium text-2A2928">Nguyễn Mạnh Cường</div>
+                                    </div>
+                                    <div class="uk-width-auto">
+                                        <div class="fz-12 text-999">10/12/2024</div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="uk-position-relative uk-text-nowrap uk-display-inline-block">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                            <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#999"/>
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                            <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#999"/>
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                            <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#999"/>
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                            <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#999"/>
+                                        </svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                            <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#999"/>
+                                        </svg>
+                                        <div class="uk-position-center-left uk-text-nowrap uk-position-z-index uk-overflow-hidden" style="width: <?= rand(0,99) ?>%">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                                <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#FFCC00"/>
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                                <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#FFCC00"/>
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                                <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#FFCC00"/>
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                                <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#FFCC00"/>
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                                <path d="M6 0L7.34708 4.1459H11.7063L8.17963 6.7082L9.52671 10.8541L6 8.2918L2.47329 10.8541L3.82037 6.7082L0.293661 4.1459H4.65292L6 0Z" fill="#FFCC00"/>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="item-4px fz-12 be-vietnam-pro-medium line-height-133 text-5E5D5A">Lorem ipsum dolor sit amet consectetur. Ac purus arcu nunc mauris adipiscing faucibus pharetra.</div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+            <div class="space-h-20"></div>
+            <nav aria-label="Pagination">
+                <ul class="pagination uk-pagination uk-flex-center uk-margin-remove-bottom">
+                    <li><a href="#"><span uk-pagination-previous></span></a></li>
+                    <li><a href="#">1</a></li>
+                    <li class="uk-disabled"><span>…</span></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">6</a></li>
+                    <li class="uk-active"><span aria-current="page">7</span></li>
+                    <li><a href="#">8</a></li>
+                    <li><a href="#"><span uk-pagination-next></span></a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="uk-width-auto">
+            <div class="uk-cover-container border-20">
+                <img src="images/slider4.jpeg" alt="" uk-cover="">
+                <canvas width="320" height="534"></canvas>
+            </div>
+        </div>
     </div>
 </div>
 <div class="space-h-100"></div>
